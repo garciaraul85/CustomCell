20235e626a3d64b9aa271ceb5ff6572b057bf7db
raulgarcia <garcia.raul@gmail.com>
2020-09-22 09:25:49 -0400

    StoryBoard and Controller connected

---
+++ CustomCell/FruitTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FruitTableViewCell.swift
+//  CustomCell
+//
+//  Created by Saulo Garcia on 9/22/20.
+//
+
+import UIKit
+
+class FruitTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var fruitImageView: UIImageView!
+    @IBOutlet weak var fruitNameLabel: UILabel!    
+    
+}
